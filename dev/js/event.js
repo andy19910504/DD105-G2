dc0da0599f2fd27017fd43e9b19e55734c320554
love00242 <love00242@gmail.com>
2020-03-04 04:28:33 +0800

    燈箱結束~~~還沒rrrrrrr加動畫qqq

--- dev/js/event.js
+++ dev/js/event.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    //發起揪團
     $(".lightbox").on("click", function () {
         $(".backDrop").animate({ "opacity": ".70" }, 500);
         $(".lightbox_holdevent_info").animate({"opacity": "1.0"}, 500);
@@ -13,5 +14,27 @@ $(document).ready(function () {
     function closeBox() {
         $(".backDrop,.lightbox_holdevent_info").animate({ "opacity": "0" }, 500);
         $(".backDrop,.lightbox_holdevent_info").css("display", "none");
+
+        //揪團詳情
+        $(".backDrop,.lightbox_detailevent_info").animate({ "opacity": "0" }, 500);
+        $(".backDrop,.lightbox_detailevent_info").css("display", "none");
+
     }
+
+    //揪團詳情
+    $(".lightEvent").on("click",function(){
+        $(".backDrop").animate({ "opacity": ".70" }, 500);
+        $(".lightbox_detailevent_info").animate({"opacity": "1.0"}, 500);
+        $(".backDrop, .lightbox_detailevent_info").css("display", "block");
+    });
+
+
+    //檢舉揪團
+    $(".reportBtn").on("click",function(){
+        $(".backDrop").animate({ "opacity": ".70" }, 500);
+        $(".lightbox_report_info").animate({"opacity": "1.0"}, 500);
+        $(".backDrop, .lightbox_report_info").css("display", "block");
+    });
+
+    
 });
