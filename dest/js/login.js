97b2d24ae86f3c09d3c139e33f067c38882211ee
redhair <redhair0828@yahoo.com.tw>
2020-03-04 21:47:07 +0800

    會員R完了(還差hover要動)

--- dest/js/login.js
+++ dest/js/login.js
@@ -11,12 +11,30 @@ $(".login_register").click(function(){
     $(".login_all_all").css("display","none");
     $(".register").attr("style","display:block");
 })
+//按登入頁的找回密碼 跳找回
+$(".findpsw").click(function(){
+    $(".login_all_all").css("display","none");
+    $(".forgetpsw").attr("style","display:block");
+})
+//按找回密碼頁的返回 跳登入頁
+$(".forgetpsw_back").click(function(){
+    $(".forgetpsw").attr("style","display:none");
+    $(".login_all_all").css("display","block");
+
+})
+//按找回註冊頁的返回 跳登入頁
+$(".register_back").click(function(){
+    $(".register").attr("style","display:none");
+    $(".login_all_all").css("display","block");
+
+})
 //按右上XX 關掉登入燈箱
 $(".login_close").click(function(){
     $(".login").attr("style","display:none");
     //跳回登入畫面
     $(".login_all_all").css("display","block");
     $(".register").attr("style","display:none");
+    $(".forgetpsw").attr("style","display:none");
 })
 // $(".login").click(function(e){
 //     e.stopPropagation();
