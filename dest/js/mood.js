032e4be08f1a5f990b7ca28a9f681cb9183dc7d9
Sihya <sylvia.taipei@gmail.com>
2020-03-10 21:16:55 +0800

    commit不用錢

--- dest/js/mood.js
+++ dest/js/mood.js
@@ -1,27 +1,27 @@
 window.addEventListener("load", function () {
-    let holdevent = document.querySelectorAll(".lightbox")
+    // let holdevent = document.querySelectorAll(".lightbox")
     let lightBoxs = document.querySelectorAll(".backDrop");
     let closes = document.querySelectorAll(".close");
 
-    let eventDetails = document.querySelectorAll(".lightDetail");
+    let moodDetails = document.querySelectorAll(".lightDetail");
 
-    let eventReports = document.querySelectorAll(".dotWrap");
-    let eventReportclose = document.querySelectorAll(".cancel");
+    let moodReports = document.querySelectorAll(".dotWrap");
+    let moodReportclose = document.querySelectorAll(".cancel");
     // 發起揪團
-    for (let i = 0; i < holdevent.length; i++) {
+    // for (let i = 0; i < holdevent.length; i++) {
 
-        holdevent[i].addEventListener("click", function () {
-            lightBoxs[i].style.display = "flex";
+    //     holdevent[i].addEventListener("click", function () {
+    //         lightBoxs[i].style.display = "flex";
            
-        });
-        closes[i].addEventListener("click", function () {
-            lightBoxs[i].style.display = "none";
-        });
-    }
+    //     });
+    //     closes[i].addEventListener("click", function () {
+    //         lightBoxs[i].style.display = "none";
+    //     });
+    // }
     //揪團詳情
-    for (let i = 0; i < eventDetails.length; i++) {
+    for (let i = 0; i < moodDetails.length; i++) {
 
-        eventDetails[i].addEventListener("click", function () {
+        moodDetails[i].addEventListener("click", function () {
             lightBoxs[1].style.display = "flex";
             
         });
@@ -31,13 +31,13 @@ window.addEventListener("load", function () {
     }
 
     //揪團檢舉
-    for (let i = 0; i < eventReports.length; i++) {
+    for (let i = 0; i < moodReports.length; i++) {
 
-        eventReports[i].addEventListener("click", function () {
+        moodReports[i].addEventListener("click", function () {
             lightBoxs[2].style.display = "flex";
             
         });
-        eventReportclose[0].addEventListener("click", function () {
+        moodReportclose[0].addEventListener("click", function () {
             lightBoxs[2].style.display = "none";
         });
     }
