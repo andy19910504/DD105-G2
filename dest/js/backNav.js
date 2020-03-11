ffff064e752b94fccc40a0d676f0123ebb78a861
benben9535 <benben9535@gmail.com>
2020-03-11 22:51:53 +0800

    新增歡迎頁、管理員登入頁、後台框架sample(使用bootstrap)

---
+++ dest/js/backNav.js
@@ -0,0 +1,8 @@
+window.addEventListener("load",function(){
+    $("#sideNavBtn").click(function(){
+        $(".sideNav").toggleClass("-sideNavOn");
+        $(".arrow").toggleClass("-turnArrow");
+    })
+
+
+})
