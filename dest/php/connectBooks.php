98ee33f1f30579b85bafed2a82c0a763c7088f08
benben9535 <benben9535@gmail.com>
2020-03-19 19:56:10 +0800

    後台--機器人管理功能完成，可以新增、修改、刪除關鍵字和回答內容。

--- dest/php/connectBooks.php
+++ dest/php/connectBooks.php
@@ -1,7 +1,7 @@
 <?php
-$dsn = "mysql:host=localhost;port=3306;dbname=books;charset=utf8";
-$user = "root";
-$password = "9527";
+$dsn = "mysql:host=localhost;port=3306;dbname=dd105g2;charset=utf8";
+$user = "dd105g2";
+$password = "dd105g2";
 $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 $pdo = new PDO($dsn, $user, $password, $options);
 ?>
