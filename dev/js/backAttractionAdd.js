2b4c02d7795f0052a5faeec333d2d2e335bcf873
jaysqer <jaysqer@gmail.com>
2020-03-24 02:11:04 +0800

    commit 細部修改新增景點的照片上傳部分

--- dev/js/backAttractionAdd.js
+++ dev/js/backAttractionAdd.js
@@ -3,7 +3,7 @@ function showPhoto() {
     let btnShow = document.getElementsByClassName("btnShow");
     for (let i = 0; i < showPhoto.length; i++) {
         btnShow[i].onchange = function (e) {
-            let file = e.target.files[i];
+            let file = e.target.files[0];
             let reader = new FileReader();
             reader.onload=function(e){
                 showPhoto[i].src=reader.result;
@@ -41,34 +41,34 @@ window.addEventListener("load", function () {
         console.log(attrStatus);
         var editForm = new FormData();
         // 檢驗
-        if (attrName == "") {
-            alert("請確實填寫名稱！");
-            return;
-        } else if (attrAddress == "") {
-            alert("請確實填寫地址！");
-            return;
-        } else if (attrInfo1 == "") {
-            alert("請確實填寫資訊1！");
-            return;
-        } else if (attrInfo2 == "") {
-            alert("請確實填寫資訊2！");
-            return;
-        } else if (attrPhoto1 == "") {
-            alert("請確實放上照片1！");
-            return;
-        } else if (attrPhoto2 == "") {
-            alert("請確實放上照片2！");
-            return;
-        } else if (attrLongitude == "") {
-            alert("請確實填寫經度！");
-            return;
-        } else if (attrLatitude == "") {
-            alert("請確實填寫緯度！");
-            return;
-        } else if (attrStatus == "") {
-            alert("請確實填寫狀態！");
-            return;
-        } else {
+        // if (attrName == "") {
+        //     alert("請確實填寫名稱！");
+        //     return;
+        // } else if (attrAddress == "") {
+        //     alert("請確實填寫地址！");
+        //     return;
+        // } else if (attrInfo1 == "") {
+        //     alert("請確實填寫資訊1！");
+        //     return;
+        // } else if (attrInfo2 == "") {
+        //     alert("請確實填寫資訊2！");
+        //     return;
+        // } else if (attrPhoto1 == "") {
+        //     alert("請確實放上照片1！");
+        //     return;
+        // } else if (attrPhoto2 == "") {
+        //     alert("請確實放上照片2！");
+        //     return;
+        // } else if (attrLongitude == "") {
+        //     alert("請確實填寫經度！");
+        //     return;
+        // } else if (attrLatitude == "") {
+        //     alert("請確實填寫緯度！");
+        //     return;
+        // } else if (attrStatus == "") {
+        //     alert("請確實填寫狀態！");
+        //     return;
+        // } else {
             // 建一個表單物件，將值放入表單物件中
 
             editForm.append('attrName', attrName);
@@ -93,7 +93,7 @@ window.addEventListener("load", function () {
             xhr.open("post", "./php/attractionAdd.php", true);
             xhr.send(editForm); // 傳送表單物件
             location.reload();
-        }
+        // }
     });
 });
 
--- dev/php/attractionAdd.php
+++ dev/php/attractionAdd.php
@@ -1,7 +1,7 @@
 <?php
-try{
-require_once("./connectBooks.php");
-$sql="
+try {
+    require_once("./connectBooks.php");
+    $sql = "
 INSERT INTO `attractions`
  (`attraction_number`, `attraction_name`, 
   `attraction_address`, `attraction_information1`,
@@ -11,22 +11,88 @@ INSERT INTO `attractions`
   (NULL, :attrName,:attrAddress, :attrInfo1, :attrInfo2, :attrPhoto1, :attrPhoto2, :attrLat,:attrLot, :attrStatus);
  ";
 
-$edit = $pdo->prepare($sql);
-// $edit->bindValue(":attrNum",$_POST["editNumber"]);
-$edit->bindValue(":attrName",$_POST["attrName"]);
-$edit->bindValue(":attrAddress",$_POST["attrAddress"]);
-$edit->bindValue(":attrInfo1",$_POST["attrInfo1"]);
-$edit->bindValue(":attrInfo2",$_POST["attrInfo2"]);
-$edit->bindValue(":attrPhoto1",$_POST["attrPhoto1"]);
-$edit->bindValue(":attrPhoto2",$_POST["attrPhoto2"]);
-$edit->bindValue(":attrLat",$_POST["attrLatitude"]);
-$edit->bindValue(":attrLot",$_POST["attrLongitude"]);
-$edit->bindValue(":attrStatus",$_POST["attrStatus"]);
+    $edit = $pdo->prepare($sql);
+    // $edit->bindValue(":attrNum",$_POST["editNumber"]);
+    $edit->bindValue(":attrName", $_POST["attrName"]);
+    $edit->bindValue(":attrAddress", $_POST["attrAddress"]);
+    $edit->bindValue(":attrInfo1", $_POST["attrInfo1"]);
+    $edit->bindValue(":attrInfo2", $_POST["attrInfo2"]);
+    $edit->bindValue(":attrPhoto1", $_POST["attrPhoto1"]);
+    $edit->bindValue(":attrPhoto2", $_POST["attrPhoto2"]);
+    $edit->bindValue(":attrLat", $_POST["attrLatitude"]);
+    $edit->bindValue(":attrLot", $_POST["attrLongitude"]);
+    $edit->bindValue(":attrStatus", $_POST["attrStatus"]);
 
-$edit->execute();
+    $edit->execute();
 
-echo "修改成功！";
-
-}catch(PDOException $e){
+    echo "修改成功！";
+} catch (PDOException $e) {
     echo $e->getMessage();
 }
+?>
+<?php
+
+switch ($_FILES['attrPhoto1']['error']) {
+    case 0:
+        if (file_exists("../img/attractions") === false) {
+            //make directory
+            mkdir("../img/attractions");
+        }
+        $from = $_FILES['attrPhoto1']['tmp_name'];
+        $to = "../img/attractions/{$_FILES['attrPhoto1']['name']}"; //images/smile.gif
+
+        if (move_uploaded_file($from,$to )) {
+            echo "上傳成功";
+        } else {
+            echo "拷貝失敗";
+        }
+        break;
+    case 1:
+        echo "上傳檔案太大, 不可超過", ini_get("upload_max_filesize"), "<br>"; // 取得ini配置的參數
+        break;
+    case 2:
+        echo "上傳檔案太大, 不可超過{$_POST["MAX_FILE_SIZE"]}<br>"; // 取得設置的隱藏欄位資訊
+        break;
+    case 3:
+        echo "上傳檔案不完整<br>";
+        break;
+    case 4:
+        echo "未選檔案<br>";
+        break;
+    default:
+        echo "error.....: ", $_FILES['attrPhoto1']['error'], "<br>"; // 發生的錯誤代碼
+}
+?>
+<?php
+
+switch ($_FILES['attrPhoto2']['error']) {
+    case 0:
+        if (file_exists("../img/attractions") === false) {
+            //make directory
+            mkdir("../img/attractions");
+        }
+        $from = $_FILES['attrPhoto2']['tmp_name'];
+        $to = "../img/attractions/{$_FILES['attrPhoto2']['name']}"; //images/smile.gif
+
+        if (move_uploaded_file($from,$to )) {
+            echo "上傳成功";
+        } else {
+            echo "拷貝失敗";
+        }
+        break;
+    case 1:
+        echo "上傳檔案太大, 不可超過", ini_get("upload_max_filesize"), "<br>"; // 取得ini配置的參數
+        break;
+    case 2:
+        echo "上傳檔案太大, 不可超過{$_POST["MAX_FILE_SIZE"]}<br>"; // 取得設置的隱藏欄位資訊
+        break;
+    case 3:
+        echo "上傳檔案不完整<br>";
+        break;
+    case 4:
+        echo "未選檔案<br>";
+        break;
+    default:
+        echo "error.....: ", $_FILES['attrPhoto2']['error'], "<br>"; // 發生的錯誤代碼
+}
+?>
