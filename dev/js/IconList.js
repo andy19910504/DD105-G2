3c83deb2519ddc3fe2e9b64783cf791d09bbedf3
andy19910504 <andy19910504@gmail.com>
2020-03-26 22:37:32 +0800

    小朱 增加從資料庫抓ICON

---
+++ dev/js/IconList.js
@@ -0,0 +1,50 @@
+function $id(id) {
+    return document.getElementById(id);
+}
+// var iconclick='';
+
+// 動態新增卡片
+function IconList(icon) {
+    let iconchoose = $id("iconchoose");
+    let iconTable = JSON.parse(icon); //把JSON字串翻譯成JS
+    let html = "";
+
+    for (i = 0; i < iconTable.length; i++) {
+        // if (eventTable[i].member_number == null) {
+        //     html += `
+        //         <div class="starMark">
+        //             <div><img src="./img/event/event_star.png" alt=""></div>
+        //             <div class="markWord">官方路線</div>
+        //         </div>
+        //         `
+        // } else {
+        //     html += `<div class="starMark"></div>`
+        // }
+        html += `
+        <div class="icon" id="icon_${iconTable[i].customized_product_number}">
+        <img src="${ iconTable[i].customized_product_url}">
+    </div>
+        `;
+    }
+    iconchoose.innerHTML = html;
+}
+
+// 確定是否有撈到資料
+function check() {
+    let xhr = new XMLHttpRequest();
+
+    xhr.onload = function () {
+        if (xhr.status == 200) {
+            //正確到php撈資料
+            IconList(xhr.responseText);
+        } else {
+            alert(xhr.status);
+        }
+
+    };
+    xhr.open("Get", "./php/IconList.php", true);
+    xhr.send(null);
+}
+
+
+window.addEventListener("load", check());
--- gulpfile.js
+++ gulpfile.js
@@ -36,19 +36,11 @@ var web = {
     ]
 }
     var options = {
-<<<<<<< HEAD
-        base: './dest', // 檔案位置
-        debug: true, 
-        bin: 'C:/php-7.4.2-nts-Win32-vc15-x64/php.exe', // php執行檔的路徑
-        ini: 'C:/php-7.4.2-nts-Win32-vc15-x64/php.ini', // php的ini檔的路徑
-        port: 8080,  // 自行定義端口
-=======
         base: './dest', /*从打包目录访问*/
         debug: true, /*调试信息*/
         bin: 'C:/php-7.4.3/php.exe',/*自本地php.exe路径地址*/
         ini: 'C:/php-7.4.3/php.ini',/*自本地php.ini路径地址*/
         port: 8080, /*自定义端口*/
->>>>>>> b01499aa3f641af252d4644b8d43cc5e5938b3e5
     };
 
 //流程
@@ -116,7 +108,7 @@ gulp.task('default', function () { // default 只要打gulp 即可執行
             proxy: 'localhost:8080', // 網址路徑必須跟php端口一樣
             port:3000,
             watch: true,
-            index: "./welcome.html"
+            index: "./postcard.html"
 
         }
     });
