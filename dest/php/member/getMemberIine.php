76b9c4e9fb680432eccbe1292d84f5599e63571b
redhair <redhair0828@yahoo.com.tw>
2020-03-27 18:57:19 +0800

    google地圖真難

---
+++ dest/php/member/getMemberIine.php
@@ -0,0 +1,23 @@
+<?php
+try {
+    require_once("connectBooks.php");
+    $memNum=$_GET["number"];
+    $sql = "select * from `routes`  where member_number=:memNum";
+    $memberline = $pdo->prepare($sql);
+    $memberline->bindvalue(":memNum",$memNum);
+    $memberline->execute();
+
+    $memberlineRow = $memberline->fetchAll(PDO::FETCH_ASSOC);
+    $sql1 = "select * from `routes` a join `custom_attraction_list`b on( a.route_number=b.route_number) join `custom_attraction`c on( b.custom_attraction_number=c.custom_attraction_number) where member_number=:memNum
+    ";
+    $memberlinelist = $pdo->prepare($sql1);
+    $memberlinelist->bindvalue(":memNum",$memNum);
+    $memberlinelist->execute();
+
+    $memberlinelistRow = $memberlinelist->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode(array("memberline"=>$memberlineRow,"memberlinelist"=>$memberlinelistRow));
+        // echo json_encode(array('attraction_name' => $attrRow, 'routeInfo' => $routesRow));
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+?>
