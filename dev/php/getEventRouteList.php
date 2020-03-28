bbe292034663ec229b7ca0a3dda71c07cac689d8
love00242 <love00242@gmail.com>
2020-03-28 17:01:43 +0800

    發揪團中...

--- dev/php/getEventRouteList.php
+++ dev/php/getEventRouteList.php
@@ -1,4 +1,7 @@
 <?php
+    session_start();
+    $member = $_SESSION["member_number"];
+
 try{
     require_once("connectBooks.php");
     // 顯示官方路線
@@ -9,19 +12,22 @@ try{
     
     $routes = $pdo->query($sql);
     $routesRow = $routes->fetchAll(PDO::FETCH_ASSOC);
-
+    // echo json_encode($member);
+   
+    // echo $member;    
     // 顯示自訂路線
     $sql = "select * 
             from `routes` r
-            join `custom_attraction_list` l on(r.route_number = l.route_number) 
-            join `custom_attraction` a on(l.custom_attraction_number =a.custom_attraction_number)
-            where  r.member_number = :origin_member_number";
+            join `custom_attraction_list` l on (r.route_number = l.route_number) 
+            join `custom_attraction` a on (l.custom_attraction_number =a.custom_attraction_number)
+            where  r.member_number =$member";
 
     $custom = $pdo->query($sql);
-    $custom ->bindValue(":origin_member_number",$_POST['origin_member_number']);
     $custom ->execute();
-
+    $custom->bindValue(":member_number",$member);
     $customRow = $custom->fetchAll(PDO::FETCH_ASSOC);
+
+    
 
     echo json_encode(array('routeInfo' => $routesRow,'customInfo'=>$customRow));
 
