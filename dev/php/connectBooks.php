e60bc2b3683387b28ff48d15677e80d17200f132
andy19910504 <andy19910504@gmail.com>
2020-03-29 22:38:17 +0800

    小朱更新CSS

--- dev/php/connectBooks.php
+++ dev/php/connectBooks.php
@@ -3,11 +3,7 @@ $dsn = "mysql:host=localhost;port=3306;dbname=dd105g2;charset=utf8";
 // $user = "dd105g2";
 // $password = "dd105g2";
 $user = "root";
-<<<<<<< HEAD
 $password = "0000";
-=======
-$password = "9527";
->>>>>>> 1999dc9ce60fb09f137823926612c848da9b7268
 $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 $pdo = new PDO($dsn, $user, $password, $options);
 ?>
--- gulpfile.js
+++ gulpfile.js
@@ -36,19 +36,11 @@ var web = {
     ]
 }
     var options = {
-<<<<<<< HEAD
         base: './dest', /*从打包目录访问*/
         debug: true, /*调试信息*/
         bin: 'C:/php-7.4.3/php.exe',/*自本地php.exe路径地址*/
         ini: 'C:/php-7.4.3/php.ini',/*自本地php.ini路径地址*/
         port: 8080, /*自定义端口*/
-=======
-        base: './dest', // 檔案位置
-        debug: true, 
-        bin: 'C:/php/php.exe', // php執行檔的路徑
-        ini: 'C:/php/php.ini', // php的ini檔的路徑
-        port: 8080,  // 自行定義端口
->>>>>>> 1999dc9ce60fb09f137823926612c848da9b7268
     };
 
 //流程
@@ -117,10 +109,6 @@ gulp.task('default', function () { // default 只要打gulp 即可執行
             port:3000,
             watch: true,
             index: "./postcard.html"
-<<<<<<< HEAD
-
-=======
->>>>>>> 1999dc9ce60fb09f137823926612c848da9b7268
         }
     });
     connectPhp.server(options); // 啟動
