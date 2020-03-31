468c97fbdd32e60aeb44c2fb0bf47745b769ef02
jaysqer <jie11042003@yahoo.com.tw>
2020-03-31 18:04:32 +0800

    commit 微調

--- dest/php/backRouteAdd.php
+++ dest/php/backRouteAdd.php
@@ -3,17 +3,17 @@ try {
     require_once("./connectBooks.php");
     // 新增路線至路線表格
     $sql = "
-    INSERT INTO `routes` 
-    (`route_number`, `route_name`,
-     `route_information`, `route_photo`,
-     `route_status`, `member_number`) VALUES
-    (NULL, :routeName, :routeInfo, :routePhoto, :routeStatus, NULL)
+    INSERT INTO `routes`
+    (`route_number`, `route_name`, `route_information`,
+     `route_photo`, `route_status`, `member_number`) 
+     VALUES (NULL, :routeName, :routeInfo, :routePhoto, :routeStatus, NULL)
     ;";
     $edit = $pdo->prepare($sql);
     $edit->bindValue(":routeName", $_POST["routeName"]);
     $edit->bindValue(":routeInfo", $_POST["routeInfo"]);
     $edit->bindValue(":routePhoto", $_FILES["routePhoto"]['name']);
     $edit->bindValue(":routeStatus", $_POST["routeStatus"]);
+
     $edit->execute();
 
     // 取得自動創號的key值
@@ -43,13 +43,18 @@ try {
             $to = "../img/routes/$newName1";
 
             if (move_uploaded_file($from, $to)) {
-                $sql3 = " update `routes` set `route_photo` = :imgName where route_number = $num ;";
+                $sql3 = "
+                update `routes` set `route_photo` = :imgName
+                where route_number = $num
+                ;";
+
                 $changeName = $pdo->prepare($sql3);
                 $changeName->bindValue(":imgName", $newName1);
                 $changeName->execute();
-                echo "上傳成功";
+
+                echo "成功";      
             } else {
-                echo "上傳失敗";
+                echo "失敗";
             }
             break;
         case 1:
@@ -64,10 +69,8 @@ try {
         default:
             echo "error.....: ", $_FILES['routePhoto']['error'], "<br>"; // 發生的錯誤代碼
     }
-    echo "修改成功!";
 
 } catch (PDOException $e) {
     echo $e->getMessage();
 }
-
 ?>
--- dev/js/backRobotGetList.js
+++ dev/js/backRobotGetList.js
@@ -1,7 +1,19 @@
+function checkRobot(keywords) {
+    let keywordTable = JSON.parse(keywords)
+    let selects = document.getElementsByClassName("satausInsert");
+    for (let i = 0; i < keywordTable.length; i++) {
+        if (keywordTable[i].keyword_status == 1) {
+            selects[i].options[1].selected = true;
+        }else{
+            selects[i].options[0].selected = true;
+        }
+    }
+}
+
 function showList(keywords) {
     let keywordWrap = document.getElementById('keywordList');
     let keywordTable = JSON.parse(keywords) //把JSON字串翻譯成JS陣列，陣列內有物件，(每個row是一個物件)
-    let html="";
+    let html = "";
 
     //根據傳回的列數動態新增表格
     for (i = 0; i < keywordTable.length; i++) {
@@ -10,11 +22,16 @@ function showList(keywords) {
             <td class="col-1">${keywordTable[i].keyword_number}</td>
             <td class="col-1"> <input type="text" class="form-control" value="${keywordTable[i].keyword_word}"> </td>
             <td class="col-7"> <input type="text" class="form-control" value="${keywordTable[i].keyword_response}"> </td>
-            <td class="col-2"> <input type="text" class="form-control" value="${keywordTable[i].keyword_status}"> </td>
+            <td class="col-2"> 
+                <select class="form-control satausInsert">
+                    <option value="0">否</option>
+                    <option value="1">是</option>
+                </select> 
+            </td>
             <td class="col-1"> <button class="btn btn-outline-success btn-sm updateBtn">儲存</button> <button class="btn btn-outline-danger btn-sm deleteBtn">刪除</button> </td>
         </tr>
         `
-        }   
+    }
     keywordWrap.innerHTML = html;
 }
 
@@ -27,6 +44,7 @@ function getKeywordList() {
         if (xhr.status == 200) {
             //到php撈所有機器人關鍵字的資料，送到showList函式中執行
             showList(xhr.responseText);
+            checkRobot(xhr.responseText);
         } else {
             alert(xhr.status + "失敗");
         }
